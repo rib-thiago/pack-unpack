8ea115d566ccc8ffc813cd076967df9ac51ffebc
Thiago Ribeiro <thiago.bernardes@aluno.ifsp.edu.br>
2023-03-09 02:22:00 -0300

    feat: suporte ao getopts
    Suporte a opções pela linha de comando para selecionar modos de uso do programa

--- compress.sh
+++ compress.sh
@@ -7,7 +7,7 @@
 ################################################################################
 
 # Description:
-# This is a bash script that allows you to compress files using various 
+# This is a bash script that allows you to compress files using various
 # compression tools, such as tar, zip, and rar.
 #
 # Author: Thiago Ribeiro
@@ -15,12 +15,12 @@
 # Version: 0.0.1
 #
 # Usage:
-# To use the script, simply run it with one or more file names as arguments. 
+# To use the script, simply run it with one or more file names as arguments.
 # If no files are specified, the script will display an error message and exit.
-# When you run the compress.sh script, it will prompt you to choose a 
-# compression tool from a menu: Once a compression tool is selected, the script 
-# will compress the files in the background using the selected tool and display 
-# a progress bar. Once the compression is completed, a successful completion 
+# When you run the compress.sh script, it will prompt you to choose a
+# compression tool from a menu: Once a compression tool is selected, the script
+# will compress the files in the background using the selected tool and display
+# a progress bar. Once the compression is completed, a successful completion
 # message will be displayed along with the name of the output file.
 #
 # Dependencies:
@@ -42,10 +42,14 @@
 # Version   Date          Author          Description
 # -------   ----          ------          -----------
 # 0.0.1     [2023-01-07]  Thiago Ribeiro  Initial release.
-# 
+# 0.0.2     [2023-01-07]  Thiago Ribeiro  Suporte ao getopts
+#
 
 # TODO:
-# Adicionar suporte ao getopts
+#  Alterar o modo de uso no cabeçalho
+#  Fazer um README.md
+#  Escrever mensagens de uso
+#  Cirar script decompress
 #
 # FIXME:
 # None.
@@ -61,42 +65,86 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Display menu to choose compression tool
-echo "Choose a compression tool:"
-echo "1) tar"
-echo "2) tar.gz"
-echo "3) tar.bz2"
-echo "4) zip"
-echo "5) rar"
-read -rp "Enter the number of your choice: " choice
+# Set the compression command and extension based on the command line options
+while getopts ":tgbzrh" opt; do
+    case $opt in
+    t)
+        cmd="tar cf"
+        ext=".tar"
+        ;;
+    g)
+        cmd="tar zcf"
+        ext=".tar.gz"
+        ;;
+    b)
+        cmd="tar jcf"
+        ext=".tar.bz2"
+        ;;
+    z)
+        cmd="zip -q -r"
+        ext=".zip"
+        ;;
+    r)
+        cmd="rar a -r -s -m5 -ep1"
+        ext=".rar"
+        ;;
+    h)
+        echo "Usage: $0 [options] file1 file2 file3 ..."
+        echo "  Options:"
+        echo "    -t  tar"
+        echo "    -g  tar.gz"
+        echo "    -b  tar.bz2"
+        echo "    -z  zip"
+        echo "    -r  rar"
+        echo "    -h  show this help message"
+        exit 0
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
+    esac
+done
+shift $((OPTIND - 1))
 
-# Set the compression command and extension based on the choice
-case $choice in
-1)
-    cmd="tar cf"
-    ext=".tar"
-    ;;
-2)
-    cmd="tar zcf"
-    ext=".tar.gz"
-    ;;
-3)
-    cmd="tar jcf"
-    ext=".tar.bz2"
-    ;;
-4)
-    cmd="zip -q -r"
-    ext=".zip"
-    ;;
-5)
-    cmd="rar a -r -s -m5 -ep1"
-    ext=".rar"
-    ;;
-*)
-    echo "Error: Invalid choice"
-    exit 1
-    ;;
-esac
+# If no options were passed, display the menu
+if [ -z "$cmd" ]; then
+    echo "Choose a compression tool:"
+    echo "1) tar"
+    echo "2) tar.gz"
+    echo "3) tar.bz2"
+    echo "4) zip"
+    echo "5) rar"
+    read -p "Enter the number of your choice: " choice
+
+    # Set the compression command and extension based on the choice
+    case $choice in
+    1)
+        cmd="tar cf"
+        ext=".tar"
+        ;;
+    2)
+        cmd="tar zcf"
+        ext=".tar.gz"
+        ;;
+    3)
+        cmd="tar jcf"
+        ext=".tar.bz2"
+        ;;
+    4)
+        cmd="zip -q -r"
+        ext=".zip"
+        ;;
+    5)
+        cmd="rar a -r -s -m5 -ep1"
+        ext=".rar"
+        ;;
+    *)
+        echo "Error: Invalid choice"
+        exit 1
+        ;;
+    esac
+fi
 
 # Set the output file name
 output_file="${PWD##*/}$ext"
@@ -110,10 +158,6 @@ while kill -0 "$pid" 2>/dev/null; do
     printf "+"
     sleep 1
 done
-
-# Display a successful completion message
-echo -e "\nCompression completed successfully"
-echo "Output file: $output_file"
 
 # Display a successful completion message
 echo -e "\nCompression completed successfully"

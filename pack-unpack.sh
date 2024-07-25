461e3a379bdeda10f4caabf0b8e5afcee76f0ce0
Thiago Ribeiro <mackandalls@gmail.com>
2024-07-25 12:36:51 -0300

    corrigindo a lógica do getopts

--- pack-unpack.sh
+++ pack-unpack.sh
@@ -1,44 +1,13 @@
 #!/usr/bin/env bash
-#
-#Script: pack-unpack.sh
-#
-# Lista, extrai ou compacta arquivos passados como parâmetros
-# após opções
-# Obs.:  Utiliza as ferramentas Tar, Gzip. Bzip2, Zip e Rar
-# Obs1.: Nesta versão, não estão disponíveis os formatos zip e rar
-# Obs2.: Para testar nesta plataforma: antes atribua a permissão de executável ao arquivo:
-#        chmod +x main.sh
-#
-#  Para executar no Console:
-#        ./main [OPÇÕES] arquivos
-#
-#
-# Versão 0.1: Primeira versão do script
-# Versão 0.2: Testando a seleção por formato de arquivo
-# Versão 0.3: Adicionando suporte à opções pela linha de comando
-# Versão 0.4: Instruções adaptadas à plataforma replit
-# Versão 0.5: Adaptado ao getopts (12/04/22)
-#
-# Autor: Thiago Ribeiro <mackandalls@gmail.com>
-# Data : 15/07/21
-#
-#
-#
 
-#
-# SELETOR=0
-
-versao (){
-
-echo -n "$(basename "$0")" 
-grep '^# Versão ' "$0" | tail -1 | cut -d : -f 1 | tr -d \#
-exit 0
-
+versao() {
+    echo -n "$(basename "$0")"
+    grep '^# Versão ' "$0" | tail -1 | cut -d : -f 2 | tr -d ' '
+    exit 0
 }
 
-
-uso (){
-  echo "
+uso() {
+    echo "
 Uso: $(basename "$0") [OPÇÕES] arq01..arqN
 
 OPÇÕES:
@@ -49,156 +18,119 @@ OPÇÕES:
   -h  Exibe este menu de ajuda e encerra programa
   -v  Exibe nome e versão do programa
 "
-  exit 0
+    exit 0
 }
 
 # Função para verificar dependências
 verificar_dependencias() {
-  local dependencias=("tar" "gzip" "bzip2" "zip" "rar" "unrar" "unzip")
-  for dep in "${dependencias[@]}"; do
-    if ! command -v "$dep" &> /dev/null; then
-      echo "Erro: Dependência '$dep' não encontrada. Instale-a e tente novamente."
-      exit 1
-    fi
-  done
+    local dependencias=("tar" "gzip" "bzip2" "zip" "rar" "unrar" "unzip")
+    for dep in "${dependencias[@]}"; do
+        if ! command -v "$dep" &> /dev/null; then
+            echo "Erro: Dependência '$dep' não encontrada. Instale-a e tente novamente."
+            exit 1
+        fi
+    done
 }
 
-# MENSAGEM_ERRO_1="
-# 
-# Insira uma opção válida.
-# Para acessar ajuda, digite
-# 
-# 	./pack-unpack.sh -h ou --help
-# 
-# "
-# 
-# MENSAGEM_ERRO_2="
-# 
-# Erro: Faltou argumento para: $OPTARG
-# 
-# Para acessar ajuda, digite
-# 
-# 	./pack-unpack.sh -h ou --help
-# "
+compactar_arquivos() {
+    local arquivos=("$@")
+    echo "${arquivos[@]}"
+    echo -e "      *Menu de opções*\n     "
+    echo "1. .Tar"
+    echo "2. .Tar.gz"
+    echo "3. .Tar.bz2"
+    echo "4. .Zip"
+    echo "5. .Rar"
+    
+    read -p "Sua opção: " option
+    read -p "Insira um nome para o arquivo a ser criado: " arqname
 
-compactar_arquivos () {
-
-	echo -e "\nModo: COMPACTAÇÃO\n"
-	read -p "${arquivo[*]}"
-
-  echo -e  "      *Menu de opções*\n     "
-  echo     "1. .Tar"
-  echo     "2. .Tar.gz"
-  echo     "3. .Tar.bz2"
-  echo     "4. .Zip"
-  echo -e  "5. .Rar\n\n"
-	
-  read -p  "Sua opção: " option
-  read -p "Insira um nome para o arquivo a ser criado: " arqname
-
-  case  $option in
-          1) tar -cvf $arqname.tar ${arquivo[@]} ;;
-          2) tar -zcvf $arqname.tar.gz  ${arquivo[@]} ;;
-          3) tar -jcvf $arqname.tar.bz2  ${arquivo[@]} ;;
-          4) zip $arqname.zip  ${arquivo[@]} ;;
-          5) rar a $arqname.rar  ${arquivo[@]} ;;
-          *) read -p "Formato não suportado"; exit 1 ;;
-  esac
-  echo "status da operação: $?"
-  read -p "Pressione enter para avançar "
+    case $option in
+        1) tar -cvf "$arqname.tar" "${arquivos[@]}" ;;
+        2) tar -zcvf "$arqname.tar.gz" "${arquivos[@]}" ;;
+        3) tar -jcvf "$arqname.tar.bz2" "${arquivos[@]}" ;;
+        4) zip "$arqname.zip" "${arquivos[@]}" ;;
+        5) rar a "$arqname.rar" "${arquivos[@]}" ;;
+        *) echo "Formato não suportado"; exit 1 ;;
+    esac
+    echo "status da operação: $?"
 }
-
 
 listar_pacote () {
-
-  read -p "Modo: LISTAGEM"
-  read -p "${arquivo[*]}"
-  
-	for file in ${arquivo[@]}
-  do
-    ext=${file: -3:3}
-    echo -e "\n arquivo: $file\n"
-    case $ext in
-            zip) unzip -l $file ;;
-            rar) unrar l $file ;;
-tar | bz2 | .gz) tar -tvf $file ;;
-              *) read -p "Formato não suportado" ; exit 1 ;;
-		esac
-		echo -e "\n"
-		read -p "Pressione enter para avançar"
-  done
-  echo -e "______________________________________________\n"
+    local arquivos=("$@")
+    echo "Modo: LISTAGEM"
+    for file in "${arquivos[@]}"; do
+        local ext="${file##*.}"
+        echo -e "\n arquivo: $file\n"
+        case $ext in
+            zip) unzip -l "$file" ;;
+            rar) unrar l "$file" ;;
+            tar | bz2 | gz) tar -tvf "$file" ;;
+            *) echo "Formato não suportado"; exit 1 ;;
+        esac
+    done
+    echo -e "______________________________________________\n"
 }
 
-descompactar_arquivos () {
+descompactar_arquivos() {
+    local arquivos=("$@")
+    echo "Modo: EXTRAÇÃO"
+    for file in "${arquivos[@]}"; do
+        local ext="${file##*.}"
+        echo -e "\n arquivo: $file\n"
+        read -p "Insira um nome para o diretório destino: " dirname
+        mkdir -p "./$dirname"
 
-  read -p "Modo: EXTRAÇÃO"
-  read -p "${arquivo[*]}"
-
-  for file in ${arquivo[@]}
-  do
-    var2=${file: -3:3}
-    echo -e "\n arquivo: $file\n"
-    read -p "Insira um nome para o diretório destino: " dirname
-    mkdir ./$dirname
-
-    case $var2 in
-      zip) unzip $file -d ./$dirname ;;
-      rar) unrar x $file ./$dirname ;;
-      tar) tar -xvf $file -C ./$dirname ;;
-      bz2) tar -jxvf $file -C ./$dirname ;;
-      .gz) tar -zxvf $file -C ./$dirname ;;
-      *) read -p "Formato não suportado" ; exit 1 ;;
-    esac
-    echo "$?"
-    read -p "Pressione enter para avançar"
-  done
-  echo -e "______________________________________________\n"
+        case $ext in
+            zip) unzip "$file" -d ./"$dirname" ;;
+            rar) unrar x "$file" ./"$dirname" ;;
+            tar) tar -xvf "$file" -C ./"$dirname" ;;
+            bz2) tar -jxvf "$file" -C ./"$dirname" ;;
+            gz) tar -zxvf "$file" -C ./"$dirname" ;;
+            *) echo "Formato não suportado"; exit 1 ;;
+        esac
+        echo "$?"
+    done
+    echo -e "______________________________________________\n"
 }
-
-
 
 # Verifica dependências
-verificar_dependencias
+# verificar_dependencias
 
-echo -e  "~pack-Unpack~ "
-
-## tratamento das variáveis que chegam da linha de comando. após o teste com $1, o comando shift retira ele da lista de argumentos
-## e o próximo argumento é testado como sendo $1. Quando acabarem os argumentos da lista, o script sai do laço while
-
-#test "$1" == 0 && echo "$MENSAGEM_ERRO_1" 
-
+# Variáveis
 SELETOR=0
-test -z "$1" && echo "$MENSAGEM_ERRO_1" 
+declare -a arquivos
 
-
-
-while getopts ":c:l:x:hv" opcoes
-do
-  case $opcoes in
-     c) SELETOR=1   ;; 
-     l) SELETOR=2   ;; 
-     x) SELETOR=3   ;; 
-     h) echo "$MENSAGEM_USO" ; exit 0 ;;
-     v) versao ;;
-     ?) echo "$MENSAGEM_ERRO_1" ; exit 0 ;;
-     :) echo "$MENSAGEM_ERRO_2" ; exit 0 ;;
-  esac
-	shift
-	arquivo="$*"
+# Tratamento de opções
+while getopts ":c:l:x:hv" opcoes; do
+    case $opcoes in
+        c) SELETOR=1 ;;
+        l) SELETOR=2 ;;
+        x) SELETOR=3 ;;
+        h) uso ;;
+        v) versao ;;
+        \?) echo "Opção inválida: -$OPTARG"; uso ;;  # Opção inválida
+        :) echo "A opção -$OPTARG requer um argumento."; uso ;;   # Falta argumento para uma opção
+    esac
 done
 
+# Remove as opções da lista de argumentos
+shift $((OPTIND - 2))
 
+# Coleta arquivos restantes após as opções
+if [ "$#" -gt 0 ]; then
+    arquivos+=("$@")
+else
+    echo "Você deve fornecer arquivos."
+    uso
+fi
 
 # Escolhe o modo de execução
 case $SELETOR in
-	1) compactar_arquivos $* ;;	#Compacta/Empacota
-	2) listar_pacote $* ;;	#Lista conteúdo	
-	3) descompactar_arquivos $* ;; #Descompacta
-#	*) echo "$MENSAGEM_ERRO" ; exit 1 ;;
+    1) compactar_arquivos "${arquivos[@]}" ;;
+    2) listar_pacote "${arquivos[@]}" ;;
+    3) descompactar_arquivos "${arquivos[@]}" ;;
 esac
 
-
-read -p "Obrigado!"
-
-exit
+echo "Obrigado!"
+exit 0
